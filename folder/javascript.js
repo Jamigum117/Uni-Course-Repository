f82ae7700da25bd27c359f799b3272c155251efb
Jamigum117 <145263495+Jamigum117@users.noreply.github.com>
2023-10-04 14:20:54 +0000

    update

--- folder/javascript.js
+++ folder/javascript.js
@@ -1,9 +1,16 @@
 
-(function () {
-	let sum = 0;
-	let arr = [1, 2, 3];
-	for (let i=1; i<arr.length; i++) {
-		sum += arr[i];
-	}
-	alert(sum);
-})();
+var msg = "Welcome to my webpage!" //Welcome to my barren javascript file!
+var talkie = "text to speech jumpscare" //very scary message
+
+function MOTD(output) {
+  alert(output)
+}
+MOTD(msg)
+
+function speak(output) {
+	let utterance = new SpeechSynthesisUtterance(output);
+	speechSynthesis.speak(utterance);
+}
+speak(talkie)
+
+//if this file seems empty, that's because I've been using it to practise javascript and not much else. I add stuff in this file to play around with it, then remove it afterwards.
